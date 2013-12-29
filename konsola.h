7a7d768bbdcfa484e5ee89c908fc80a164753f18
Pit16 <piotruss.168@gmail.com>
2013-12-29 16:20:05 +0100

    zadeklarowanie funkcji w konsoli

--- konsola.h
+++ konsola.h
@@ -1,1 +1,9 @@
 //konsola.h
+// do³¹czenie w main sygnatury funkcji
+
+/* funkcje: np/ pobierz liczbe ci¹gów; pobierz szyfrogramy
+ *
+ */
+
+int pobierz_liczbe_ciagow();
+int pobierz_szyfrogramy(szyfrogram* tablica_szyfrogramow);
